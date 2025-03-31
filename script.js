a41f1bb5c4a06d2b9d8c4e152afac0d3144c56c7
Jean Dalpra <03497876038@senacrs.edu.br>
2025-03-31 09:43:03 -0300

    final

---
+++ script.js
@@ -0,0 +1,5 @@
+const btnLiga = document.getElementById('ligar');
+btnLiga.addEventListener('click', () => {
+
+    document.getElementsByClassName('salamaleiku').innerHTML = 'https://static.vecteezy.com/system/resources/thumbnails/027/389/194/small_2x/graffiti-lamp-bulb-with-human-brain-inside-idea-generation-brainstorm-concept-ai-generative-free-png.png';
+    });
